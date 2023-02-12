e6f4099b3eba2e39eec34e9eefae5dbeae7971eb
Mariann Tapfer <tapferm@gmail.com>
2023-02-12 22:09:34 +0200

    formatting fixes

--- scripts/api.js
+++ scripts/api.js
@@ -1,38 +1,38 @@
-const https = require("https");
-const fs = require("fs");
-require("dotenv").config();
+const https = require('https');
+const fs = require('fs');
+require('dotenv').config();
 const github_token = process.env.github_token;
 const projectNumber = 1;
-const user = "marianntapfer";
-const filePath = "scripts/data/project.json";
+const user = 'marianntapfer';
+const filePath = 'scripts/data/project.json';
 
 let options = {
-  method: "POST",
-  hostname: "api.github.com",
-  path: "/graphql",
+  method: 'POST',
+  hostname: 'api.github.com',
+  path: '/graphql',
   headers: {
     Authorization: `Bearer ${github_token}`,
-    "Content-Type": "application/json",
-    Cookie: "_octo=GH1.1.1334685627.1652968470; logged_in=no",
-    "User-Agent": "PostmanRuntime/7.29.0",
+    'Content-Type': 'application/json',
+    Cookie: '_octo=GH1.1.1334685627.1652968470; logged_in=no',
+    'User-Agent': 'PostmanRuntime/7.29.0',
   },
 };
 
 const req = https.request(options, (res) => {
   let chunks = [];
 
-  res.on("data", (chunk) => {
+  res.on('data', (chunk) => {
     chunks.push(chunk);
   });
 
-  res.on("end", (chunk) => {
+  res.on('end', (chunk) => {
     let body = Buffer.concat(chunks);
     // console.log(body.toString());
-    fs.writeFileSync("scripts/data/project.json", body.toString());
-    console.log("wrote project data indata/project.json");
+    fs.writeFileSync('scripts/data/project.json', body.toString());
+    console.log('wrote project data indata/project.json');
   });
 
-  res.on("error", (error) => {
+  res.on('error', (error) => {
     console.error(error);
   });
 });
@@ -101,7 +101,7 @@ let postData = JSON.stringify({
     }
   }`,
   variables: {
-    login: "marianntapfer",
+    login: 'marianntapfer',
     number: 1,
     itemsFirst: 30,
     fieldValuesFirst: 10,
